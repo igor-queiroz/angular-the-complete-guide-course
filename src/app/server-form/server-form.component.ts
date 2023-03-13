ef014c4a5694bdebcc302c864c07f272b14862c7
Igor Queiroz <igor.queiroz@pagar.me>
2023-03-13 01:29:27 -0300

    [feature] added directives

--- src/app/server-form/server-form.component.ts
+++ src/app/server-form/server-form.component.ts
@@ -9,12 +9,26 @@ export class ServerFormComponent implements OnInit {
   public disableButton = true;
   public serverList: string[] = [''];
   public serverName = '';
+  public serverParagraphClassStyles = {
+    denied: false,
+    success: true,
+  };
+  public inlineStyle = 'text-bg-success';
 
   ngOnInit() {
     this.buttonCooldown();
   }
 
+  public setInlineStyle() {
+    this.inlineStyle = Math.random() < 0.5 ? 'green' : 'red';
+    return this.inlineStyle;
+  }
+
   public addServer() {
+    this.serverParagraphClassStyles = {
+      success: Math.random() < 0.5,
+      denied: Math.random() < 0.5,
+    };
     this.serverList?.push(this.serverName);
   }
 
--- src/app/server-lists/server-lists.component.ts
+++ src/app/server-lists/server-lists.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server-lists',
   templateUrl: './server-lists.component.html',
-  styleUrls: ['./server-lists.component.scss']
+  styleUrls: ['./server-lists.component.scss'],
 })
 export class ServerListsComponent {
-
+  server = '';
 }
